86ab4c8dd3dff30eeaeafe9d0cb79c85ec2b29db
DSL <sanchezlealdaniel@gmail.com>
2018-03-13 11:55:54 +0100

    class Line.

--- src/Line.cpp
+++ src/Line.cpp
@@ -1,1 +1,24 @@
 #include "Line.h"
+
+Line::Line(string id) : id(id){}
+
+void Line::addStop(Stop stop)
+{
+    stops.push_back(stop);
+}
+
+Stop Line::getStop(int i)
+{
+    return stops[i];
+}
+
+Stop* Line::find(string name)
+{
+    vector<Stop>::iterator i;
+    for (i = stops.begin(); i < stops.end(); i++) {
+        if (i->getName() == name){
+            return &(*i);
+        }
+    }
+    return NULL;
+}
--- src/Line.h
+++ src/Line.h
@@ -1,9 +1,18 @@
 #ifndef LINE_H_
 #define LINE_H_
 
+#include "Stop.h"
+#include <vector>
+
 class Line
 {
-
+    string id;
+    vector<Stop> stops;
+public:
+    Line(string id);
+    void addStop(Stop stop);
+    Stop getStop(int i);
+    Stop* find(string name);
 };
 
 #endif
