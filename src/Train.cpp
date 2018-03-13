823e61a14e1cb1b439ca42129597a409d8a55c53
Nacho <ignaciomorenoadt@gmail.com>
2018-03-13 11:25:22 +0100

    train update, makefile

--- src/Train.cpp
+++ src/Train.cpp
@@ -1,1 +1,27 @@
 #include "Train.h"
+
+Train::Train(int currentStop, float speed, Line line)
+{
+    
+}
+
+
+float Train::getSpeed()
+{
+    return speed;
+}
+
+int Train::getCurrentStop()
+{
+    return currentStop;
+}
+
+void Train::go(float desiredSpeed)
+{
+    speed = desiredSpeed;
+}
+
+void Train::stop()
+{
+    speed = 0;
+}
--- src/Train.h
+++ src/Train.h
@@ -1,9 +1,20 @@
 #ifndef TRAIN_H_
 #define TRAIN_H_
 
+#include "Line.h"
+
 class Train
 {
-
+    int currentStop;
+    float speed;
+    Line line;
+public:
+    Train();
+    Train(int currentStop, float speed, Line line);
+    float getSpeed();
+    int getCurrentStop();
+    void go(float desiredSpeed);
+    void stop();
 };
 
 #endif
