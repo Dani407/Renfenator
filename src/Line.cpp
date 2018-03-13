1996565449d1903767bd42844f1b138b703825fa
DSL <sanchezlealdaniel@gmail.com>
2018-03-13 14:42:00 +0100

    minor fixes

--- src/Line.cpp
+++ src/Line.cpp
@@ -4,26 +4,6 @@ Line::Line(){}
 
 Line::Line(string id) : id(id){}
 
-void Line::init()
-{
-    Stop stop("Aranjuez", 10, 0);
-    addStop(stop);
-    Stop stop("Ciempozuelos", 20, 1);
-    addStop(stop);
-    Stop stop("Valdemoro", 30, 2);
-    addStop(stop);
-    Stop stop("Pinto", 10, 3);
-    addStop(stop);
-    Stop stop("GetafeInd", 10, 4);
-    addStop(stop);
-    Stop stop("ElCasar", 10, 5);
-    addStop(stop);
-    Stop stop("SanCristobal", 10, 6);
-    addStop(stop);
-    Stop stop("Villaverde", 10, 7);
-    addStop(stop);
-}
-
 void Line::addStop(Stop stop)
 {
     stops.push_back(stop);
