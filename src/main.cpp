a9845529bdb35f990f17962d3018b8c08cb6e46a
DSL <sanchezlealdaniel@gmail.com>
2018-03-13 14:38:35 +0100

    init for testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,13 @@
 
 int main() {
     Train train1;
-    // train1.go(20);
+    Line C3("C3");
+    C3.addStop(Stop("a", 10, 0));
+    C3.addStop(Stop("b", 10, 1));
+    C3.addStop(Stop("c", 10, 2));
+    C3.addStop(Stop("d", 10, 3));
+    C3.addStop(Stop("e", 10, 4));
+    C3.addStop(Stop("f", 10, 5));
+    C3.addStop(Stop("g", 10, 6));
+    C3.addStop(Stop("h", 10, 7));
 }
